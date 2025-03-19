a6f579db701a0b1481c1703079f243d3072d1725
AaronL98-continuum <me@aaron.dog>
2025-03-19 21:19:35 +0000

    New route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Checkout from "../pages/Checkout.vue";
+import OrderComplete from "../pages/OrderComplete.vue";
 
 const routes = [
     {
         path: "/",
         name: "Checkout",
         component: Checkout,
+    },
+    {
+        path: "/complete/:transactionId",
+        name: "Complete",
+        component: OrderComplete,
+        props: true,
     },
 ];
 
